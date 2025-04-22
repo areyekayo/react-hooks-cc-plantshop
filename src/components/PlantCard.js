3829461d602bb666f0cdadf8c67765fb46c82adb
Riko <riko.fluchel@gmail.com>
2025-04-22 07:06:05 -0400

    add out of stock click

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState } from "react";
 
 function PlantCard({name, image, price}) {
+  const [isInStock, setIsInStock] = useState(true)
+
+  function handleInStockClick() {
+    setIsInStock(!isInStock)
+  }
+
   return (
     <li className="card" data-testid="plant-item">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button className="primary" onClick={handleInStockClick}>In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
